b879d4f3b18e84dfd05fedf717eb905c7f8e4a55
DimitarD <dimitardobrikov77@gmail.com>
2018-10-08 09:13:26 +0000

    pie chart correction

--- carnew.js
+++ carnew.js
@@ -156,8 +156,8 @@ function show_sales_for_a_year(ndx) {
           .externalLabels(50)
           .externalRadiusPadding(50)
           .drawPaths(true)
-          .dimension(runDimension)
-          .group(speedSumGroup)
+          .dimension(monthDimension)
+          .group(quantitySumGroup)
           .legend(dc.legend());
       chart.on('pretransition', function(chart) {
           chart.selectAll('.dc-legend-item text')
